172f8a74418cd26715937b42ac659d239bb1f440
nithinbairu <113778598+nithinbairu@users.noreply.github.com>
2025-09-17 20:00:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+document.getElementById("submit").addEventListener("click",
+	function(event){
+		event.preventDefault();
+		const gridid=document.getElementById("input").value;
+		const color=document.getElementById("color").value;
+
+		const allgrid=document.querySelectorAll(".grid");
+		allgrid.forEach(item=>{
+			item.style.backgroundColor='transparent'
+		})
+		const selectgrid=document.getElementById(gridid)
+		if(selectgrid){
+			selectgrid.style.backgroundColor=color;
+		}else{
+			alert("Invalid grid number! Please enter a number between 1 and 9.")
+		}
+	})
